d8f08dac9c0a3690b543cb3b9a70d414ab64ea9b
Renee Wysocki <reneew311@gmail.com>
2018-12-04 16:04:24 -0600

    initial styling & templating

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,10 +11,19 @@ module.exports = function(app) {
     });
   });
 
+    // Load example page and pass in an example by id
+    app.get("/murder", function(req, res) {
+      db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+        res.render("murder", {
+          example: dbExample
+        });
+      });
+    });
+
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
+  app.get("/murder/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
+      res.render("murder", {
         example: dbExample
       });
     });
