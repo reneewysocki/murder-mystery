dfd49facf73547f9fdd0f0ce900c1cedd6623712
Paul Coroneos <paulacoroneos@gmail.com>
2018-12-04 19:30:42 -0600

    more fixes for linter

--- public/js/index.js
+++ public/js/index.js
@@ -1,99 +1,98 @@
 // Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
+const $exampleText = $('#example-text');
+const $exampleDescription = $('#example-description');
+const $submitBtn = $('#submit');
+const $exampleList = $('#example-list');
 
 // The API object contains methods for each kind of request we'll make
-var API = {
-    saveExample: function(example) {
-        return $.ajax({
-            headers: {
-                "Content-Type": "application/json"
-            },
-            type: "POST",
-            url: "api/examples",
-            data: JSON.stringify(example)
-        });
-    },
-    getExamples: function() {
-        return $.ajax({
-            url: "api/examples",
-            type: "GET"
-        });
-    },
-    deleteExample: function(id) {
-        return $.ajax({
-            url: "api/examples/" + id,
-            type: "DELETE"
-        });
-    }
+const API = {
+  saveExample: example =>
+    $.ajax({
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      type: 'POST',
+      url: 'api/examples',
+      data: JSON.stringify(example),
+    }),
+  getExamples() {
+    return $.ajax({
+      url: 'api/examples',
+      type: 'GET',
+    });
+  },
+  deleteExample(id) {
+    return $.ajax({
+      url: `api/examples/${id}`,
+      type: 'DELETE',
+    });
+  },
 };
 
 // refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-    API.getExamples().then(function(data) {
-        var $examples = data.map(function(example) {
-            var $a = $("<a>")
-                .text(example.text)
-                .attr("href", "/example/" + example.id);
+const refreshExamples = function() {
+  API.getExamples().then(data => {
+    const $examples = data.map(example => {
+      const $a = $('<a>')
+        .text(example.text)
+        .attr('href', `/example/${example.id}`);
 
-      var $li = $("<li>")
-                .attr({
-                    class: "list-group-item",
-                    "data-id": example.id
-                })
-                .append($a);
+      const $li = $('<li>')
+        .attr({
+          class: 'list-group-item',
+          'data-id': example.id,
+        })
+        .append($a);
 
-      var $button = $("<button>")
-                .addClass("btn btn-danger float-right delete")
-                .text("ｘ");
+      const $button = $('<button>')
+        .addClass('btn btn-danger float-right delete')
+        .text('ｘ');
 
       $li.append($button);
 
       return $li;
-        });
+    });
 
     $exampleList.empty();
-        $exampleList.append($examples);
-    });
+    $exampleList.append($examples);
+  });
 };
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
-    event.preventDefault();
+const handleFormSubmit = function(event) {
+  event.preventDefault();
 
-    var example = {
-        text: $exampleText.val().trim(),
-        description: $exampleDescription.val().trim()
-    };
+  const example = {
+    text: $exampleText.val().trim(),
+    description: $exampleDescription.val().trim(),
+  };
 
-    if (!(example.text && example.description)) {
-        alert("You must enter an example text and description!");
-        return;
-    }
+  if (!(example.text && example.description)) {
+    alert('You must enter an example text and description!');
+    return;
+  }
 
-    API.saveExample(example).then(function() {
-        refreshExamples();
-    });
+  API.saveExample(example).then(() => {
+    refreshExamples();
+  });
 
-    $exampleText.val("");
-    $exampleDescription.val("");
+  $exampleText.val('');
+  $exampleDescription.val('');
 };
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-    var idToDelete = $(this)
-        .parent()
-        .attr("data-id");
+const handleDeleteBtnClick = function() {
+  const idToDelete = $(this)
+    .parent()
+    .attr('data-id');
 
-    API.deleteExample(idToDelete).then(function() {
-        refreshExamples();
-    });
+  API.deleteExample(idToDelete).then(() => {
+    refreshExamples();
+  });
 };
 
 // Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+$submitBtn.on('click', handleFormSubmit);
+$exampleList.on('click', '.delete', handleDeleteBtnClick);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,24 @@
-var db = require("../models");
+const db = require('../models');
 
 module.exports = function(app) {
-    // Get all examples
-    app.get("/api/examples", function(req, res) {
-        db.Example.findAll({}).then(function(dbExamples) {
-            res.json(dbExamples);
-        });
+  // Get all examples
+  app.get('/api/examples', (req, res) => {
+    db.Example.findAll({}).then(dbExamples => {
+      res.json(dbExamples);
     });
+  });
 
   // Create a new example
-    app.post("/api/examples", function(req, res) {
-        db.Example.create(req.body).then(function(dbExample) {
-            res.json(dbExample);
-        });
+  app.post('/api/examples', (req, res) => {
+    db.Example.create(req.body).then(dbExample => {
+      res.json(dbExample);
     });
+  });
 
   // Delete an example by id
-    app.delete("/api/examples/:id", function(req, res) {
-        db.Example.destroy({ where: { id: req.params.id } }).then(function(
-            dbExample
-        ) {
-            res.json(dbExample);
-        });
+  app.delete('/api/examples/:id', (req, res) => {
+    db.Example.destroy({ where: { id: req.params.id } }).then(dbExample => {
+      res.json(dbExample);
     });
+  });
 };
