9866177fddf5d9acfbf2634b51074fd7c618fbae
Paul Coroneos <paulalexandercoroneos@gmail.com>
2018-12-05 18:37:05 -0600

    push what i've got so group isnt too far behind

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: 'root',
     password: process.env.MYSQL_PASSWORD || null,
-    database: 'murder_db',
+    database: 'murderdb',
     host: 'localhost',
     dialect: 'mysql'
   },
